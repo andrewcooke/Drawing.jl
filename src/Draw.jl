a26a0411b47df69371ec6f6e75eb752a3550a72b
andrew cooke <andrew@acooke.org>
2015-08-30 20:40:00 -0300

    output via scope

--- src/Draw.jl
+++ src/Draw.jl
@@ -5,67 +5,62 @@ import Cairo; const X = Cairo
 import Graphics; const G = Graphics
 import Colors; const C = Colors
 
-export with, Paper, Pen, move, line, save
+export with, Paper, File, Pen, move, line, save
 
-abstract Scope
+
+
+# --- global / thread-local state
 
 typealias NullableContext Nullable{X.CairoContext}
 
 # this needs to be in a thread local variable once such things exist in julia
 type ThreadContext
-    context::Nullable{X.CairoContext}
+    context::NullableContext
     ThreadContext() = new(NullableContext())
 end
 
 thread_context = ThreadContext()
 current_context() = get(thread_context.context)
 
-# TODO - support other types
-parse_color(c) = parse(C.Colorant, c)
 
+# --- scoping infrastructure
+
+"""
+Scoped values, used with `with() do ... end`, to define some property of 
+the drawing within the do block.  All Scope subtypes must implement 
+`rank()` so that they can be sorted correctly within `with()`.
+"""
+abstract Scope
+
+"""
+Call `enter()` for each Scope, then execute the do block, and finally call
+`exit()` for each Scope.
+"""
 function with(f, scopes::Scope...)
+    # stable sort, respecting user where possible
+    s = sort!([scopes...], by=rank, alg=InsertionSort)
     c = thread_context
-    for scope in scopes
+    for scope in s
         enter(c, scope)
     end
     f()
-    for scope in reverse(scopes)
+    for scope in reverse(s)
         exit(c, scope)
     end
 end
 
-type Pen <: Scope
-    foreground::C.Color
-    width::Float64
-end
 
-Pen(foreground; width=0.02) = Pen(parse_color(foreground), width)
 
-function enter(c, p::Pen)
-    ctx = current_context()
-    X.set_source(ctx, p.foreground)
-    w = [p.width, p.width]
-    X.user_to_device_distance!(ctx, w)
-    w = maximum(map(abs, w))
-    X.set_line_width(ctx, w)
-end
+# --- utilities
 
-function exit(c, p::Pen)
-    ctx = current_context()
-    X.stroke(ctx)
-end
+parse_color(c::AbstractString) = parse(C.Colorant, c)
+parse_color(c::C.Color) = c
 
-type Paper <: Scope
-    nx::Int
-    ny::Int
-    xl::Float64
-    xr::Float64
-    yb::Float64
-    yt::Float64
-    background::C.Color
-    Paper(nx, ny, xl, xr, yb, yt, bg) = new(nx, ny, xl, xr, yb, yt, bg)
-    previous_context::NullableContext
-end
+int(x) = round(Int, x)
+
+
+
+# --- paper declaration
 
 immutable Orientation enumeration end
 const LANDSCAPE = Orientation(1)
@@ -79,7 +74,17 @@ function paper_size(size::AbstractString)
     end
 end
 
-int(x) = round(Int, x)
+type Paper <: Scope
+    nx::Int
+    ny::Int
+    xl::Float64
+    xr::Float64
+    yb::Float64
+    yt::Float64
+    background::C.Color
+    Paper(nx, ny, xl, xr, yb, yt, bg) = new(nx, ny, xl, xr, yb, yt, bg)
+    previous_context::NullableContext
+end
 
 function Paper(size::AbstractString; dpi=300::Int, background="white", 
                orientation=LANDSCAPE::Orientation, border=0.1::Float64,
@@ -96,6 +101,8 @@ function Paper(size::AbstractString; dpi=300::Int, background="white",
     end
 end
 
+rank(::Paper) = 10
+
 function enter(c, p::Paper)
     ctx = X.CairoContext(X.CairoRGBSurface(p.nx, p.ny))
     p.previous_context = c.context
@@ -109,8 +116,64 @@ function enter(c, p::Paper)
     X.set_coords(ctx, 0, 0, p.nx, p.ny, p.xl, p.xr, p.yt, p.yb)
 end
 
-# TODO - replace context
-exit(c, p::Paper) = nothing
+exit(c, p::Paper) = c.context = p.previous_context
+
+
+
+# --- output (file, display, etc)
+
+type File <: Scope
+    path::AbstractString
+end
+
+rank(::File) = 20
+
+enter(c, ::File) = nothing
+
+# TODO - other formats
+function exit(c, f::File)
+    ctx = get(c.context)
+    X.write_to_png(ctx.surface, f.path)
+end
+
+
+
+# --- line plotting
+
+type Pen <: Scope
+    foreground::C.Color
+    width::Float64
+end
+
+Pen(foreground; width=-1) = Pen(parse_color(foreground), width)
+
+rank(::Pen) = 30
+
+function enter(c, p::Pen)
+    ctx = get(c.context)
+    X.set_source(ctx, p.foreground)
+
+    if p.width >= 0
+        # width is in user coords, so scale to device coords
+        v = [p.width, p.width]
+        X.user_to_device_distance!(ctx, v)
+        w = maximum(map(abs, v))
+        X.set_line_width(ctx, w)
+    else
+        # use 2% of smaller dimension
+        d = min(ctx.surface.width, ctx.surface.height)
+        X.set_line_width(ctx, d * 0.02)
+    end
+end
+
+function exit(c, p::Pen)
+    ctx = get(c.context)
+    X.stroke(ctx)
+end
+
+
+
+# --- paths
 
 macro lift(new, old)
     :($(esc(new))(args...) = $(esc(old))(current_context(), args...))
@@ -119,10 +182,7 @@ end
 @lift(move, X.move_to)
 @lift(line, X.line_to)
 
-# TODO - other formats
-function save(path::AbstractString)
-    X.write_to_png(current_context().surface, path)
-end
+
 
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,10 @@ using Draw
 
 using Base.Test
 
-with(Paper("a4")) do
-    with(Pen("red")) do
-        move(0.0, 0.0)
-        line(1.0, 0.0)
-        line(1.0, 1.0)
-        line(0.0, 1.0)
-        line(0.0, 0.0)
-    end
-    # TODO - move into "with"
-    save("foo.png")
+with(File("foo.png"), Paper("a4"), Pen("red")) do
+    move(0.0, 0.0)
+    line(1.0, 0.0)
+    line(1.0, 1.0)
+    line(0.0, 1.0)
+    line(0.0, 0.0)
 end
