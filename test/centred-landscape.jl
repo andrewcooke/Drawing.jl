5a5a584d0bfa5c055593cb353326349c8a3d643e
andrew cooke <andrew@acooke.org>
2015-09-05 18:11:33 -0300

    docs

--- test/centred-landscape.jl
+++ test/centred-landscape.jl
@@ -1,5 +1,10 @@
 
-draw(File("centred-landscape.png"), Paper(140, 100; centred=true, background="lightgrey")) do
+draw(File("centred-landscape.png"), 
+     Paper(140, 100; centred=true, background="lightgrey"),
+     # default Pen is 0.02.  use 0.04 here so consistent with bottom
+     # left image which is twice as dark because the user coordinates
+     # are half as large.
+     Pen(0.04)) do
     move(0, 0)
     line(1, 0)
     move(0, 0)
