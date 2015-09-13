e8cf20b1193bf86f5c64d0e7f2ab969ab38c09e9
andrew cooke <andrew@acooke.org>
2015-09-13 17:05:28 -0300

    improve typing

--- src/cairo.jl
+++ src/cairo.jl
@@ -2,6 +2,10 @@
 # at some point these should be a pull request for Cairo.jl
 # WARNING: verbs have been moved to the front of function names
 
+# this is a #define - no idea how to extract it dynamically
+# https://developer.gnome.org/pango/stable/pango-Glyph-Storage.html#PANGO-SCALE:CAPS
+const PANGO_SCALE = 1024
+
 function has_current_point(ctx::X.CairoContext)
     ccall((:cairo_has_current_point, X._jl_libcairo), Int, (Ptr{Void},), ctx.ptr) != 0
 end
@@ -14,46 +18,59 @@ function get_current_point(ctx::X.CairoContext)
     (x[1], y[1])
 end
 
+immutable FontMap
+    ptr::Ptr{Void}
+end
+
 function get_font_map_default()
-    ccall((:pango_cairo_font_map_get_default, X._jl_libpangocairo), Ptr{Void}, ())
+    FontMap(ccall((:pango_cairo_font_map_get_default, X._jl_libpangocairo), Ptr{Void}, ()))
+end
+
+immutable FontFamily
+    ptr::Ptr{Void}
 end
 
 # this assumes the underlying structs that are pointed to are
 # persistent which seems to be implied by the api.  so we copy the
 # array of pointers (maybe that could be avoied by calling
 # pointer_to_array with true)
-function list_font_map_families(fm)
+function list_families(fm::FontMap)
     pff = Ptr{Ptr{Void}}[0]
     pn = Cint[0]
-    ccall((:pango_font_map_list_families, X._jl_libpango), Void, (Ptr{Void}, Ptr{Ptr{Ptr{Void}}}, Ptr{Cint}), fm, pff, pn)
+    ccall((:pango_font_map_list_families, X._jl_libpango), Void, (Ptr{Void}, Ptr{Ptr{Ptr{Void}}}, Ptr{Cint}), fm.ptr, pff, pn)
     a = pointer_to_array(pff[1], (pn[1],), false)
-    ff = copy(a)
+    ff = [FontFamily(p) for p in a]
     Libc.free(pff[1])
     ff
 end
 
-function get_font_family_name(ff)
-    bytestring(ccall((:pango_font_family_get_name, X._jl_libpango), Cstring, (Ptr{Void},), ff))
+function get_name(ff::FontFamily)
+    bytestring(ccall((:pango_font_family_get_name, X._jl_libpango), Cstring, (Ptr{Void},), ff.ptr))
 end
 
-function is_font_family_monospace(ff)
-    convert(Bool, ccall((:pango_font_family_is_monospace, X._jl_libpango), Cint, (Ptr{Void},), ff))
+function is_monospace(ff::FontFamily)
+    convert(Bool, ccall((:pango_font_family_is_monospace, X._jl_libpango), Cint, (Ptr{Void},), ff.ptr))
+end
+
+immutable FontFace
+    ptr::Ptr{Void}
 end
 
 # this assumes the underlying structs that are pointed to are
 # persistent which seems to be implied by the api.  so we copy the
 # array of pointers (maybe that could be avoied by calling
 # pointer_to_array with true)
-function list_font_family_faces(ff)
+function list_faces(ff::FontFamily)
     pfc = Ptr{Ptr{Void}}[0]
     pn = Cint[0]
-    ccall((:pango_font_family_list_faces, X._jl_libpango), Void, (Ptr{Void}, Ptr{Ptr{Ptr{Void}}}, Ptr{Cint}), ff, pfc, pn)
+    ccall((:pango_font_family_list_faces, X._jl_libpango), Void, (Ptr{Void}, Ptr{Ptr{Ptr{Void}}}, Ptr{Cint}), ff.ptr, pfc, pn)
     a = pointer_to_array(pfc[1], (pn[1],), false)
-    fc = copy(a)
+    fc = [FontFace(p) for p in a]
     Libc.free(pfc[1])
     fc
 end
 
-function get_font_face_name(fc)
-    bytestring(ccall((:pango_font_face_get_face_name, X._jl_libpango), Cstring, (Ptr{Void},), fc))
+function get_name(fc::FontFace)
+    bytestring(ccall((:pango_font_face_get_face_name, X._jl_libpango), Cstring, (Ptr{Void},), fc.ptr))
 end
+
--- test/experiments/list_fonts.jl
+++ test/experiments/list_fonts.jl
@@ -2,11 +2,11 @@
 using Drawing; const D = Drawing
 
 fm = D.get_font_map_default()
-ff = D.list_font_map_families(fm)
+ff = D.list_families(fm)
 for f in ff
-    println("$(D.get_font_family_name(f)) $(D.is_font_family_monospace(f))")
-    fc = D.list_font_family_faces(f)
+    println("$(D.get_name(f)) $(D.is_monospace(f))")
+    fc = D.list_faces(f)
     for c in fc
-        println("  $(D.get_font_face_name(c))")
+        println("  $(D.get_name(c))")
     end
 end
