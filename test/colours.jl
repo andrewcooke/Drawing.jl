aa2893506e54a3c000c83863d8669372624d460d
andrew cooke <andrew@acooke.org>
2015-09-30 19:42:11 -0300

    pretty example

--- test/colours.jl
+++ test/colours.jl
@@ -19,18 +19,18 @@ with(
      Translate(0.9, 0.9), Scale(3),
      Paper("black"),
      Pen(width; cap="round")) do
-    for x in shuffle!([i / n for i in 1:n])
-        hue = x * 360
-        theta1 = deg2rad(rotn + x*360)
-        theta2 = theta1 + deg2rad(d1)
-        theta3 = theta1 + deg2rad(d2)
-        draw(Ink(HSVA(hue, sat, val + (1-val)*rand(), alpha))) do
-            move(r1 * cos(-theta1), r1  * sin(-theta1))
-            line(r2 * cos(-theta2), r2  * sin(-theta2))
-            move(r1 * cos(-theta1), r1  * sin(-theta1))
-            line(r3 * cos(-theta3), r3  * sin(-theta3))
-        end
-    end
+     for x in shuffle!([i / n for i in 1:n])
+         hue = x * 360
+         theta1 = deg2rad(rotn + x*360)
+         theta2 = theta1 + deg2rad(d1)
+         theta3 = theta1 + deg2rad(d2)
+         draw(Ink(HSVA(hue, sat, val + (1-val)*rand(), alpha))) do
+             move(r1 * cos(-theta1), r1  * sin(-theta1))
+             line(r2 * cos(-theta2), r2  * sin(-theta2))
+             move(r1 * cos(-theta1), r1  * sin(-theta1))
+             line(r3 * cos(-theta3), r3  * sin(-theta3))
+         end
+     end
 end
 ignore || compare("colour-ring.png")
 
