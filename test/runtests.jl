6a8fce41920879b57f74fd444b36fe58f9f203d5
andrew cooke <andrew@acooke.org>
2015-09-30 09:47:40 -0300

    update test to use colour difference (was getting errors between machines)

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,18 +3,26 @@ using Drawing
 
 using Base.Test
 using SHA: sha1
+using Images
+using Colors
 
 include("cairo.jl")
 
+MAX_DIFF = 8
+
 function compare(name)
     print("$(name): ")
-    h1 = open(name, "r") do f
-        sha1(readall(f))
+    im1 = imread(name)
+    im2 = imread("target/$(name)")
+    @test size(im1) == size(im2)
+    for c in zip(im1, im2)
+        c1, c2 = c
+        d = colordiff(c1, c2)
+        if d >= MAX_DIFF
+            println("$c1 $c2 $(colordiff(c1, c2))")
+            @test colordiff(c1, c2) < MAX_DIFF
+        end
     end
-    h2 = open("target/$(name)", "r") do f
-        sha1(readall(f))
-    end
-    @test h1 == h2
     println("ok")
 end    
 
